cb2225190a08718fc1ae7a079cd1d979222ead16
AlexeyBakin <blackacsel123@gmail.com>
2022-02-13 21:38:01 +0300

    Ничерта не работает

--- src/main/java/com/example/web/controller/UserController.java
+++ src/main/java/com/example/web/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.example.web.controller;
 
 import com.example.web.model.User;
-import com.example.web.service.UserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,17 +9,9 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
     @GetMapping
-    public String getUserInfo(@AuthenticationPrincipal UserDetails userDetails,
-                              Model model){
-        String username = userDetails.getUsername();
-        User user = userService.getUserByUsername(username);
+    public String getUserInfo(@AuthenticationPrincipal User user,
+                              Model model) {
         model.addAttribute("user", user);
         return "user";
     }
